2788e7786d9f38bc335a34bbdf0a44cc0283a8e3
Michelle <michelle.liran@gmail.com>
2021-05-13 20:02:21 -0500

    tried to add some interfacing, and it will not work

--- javascript.js
+++ javascript.js
@@ -1,8 +1,17 @@
+/*
+
+Create three buttons, one for each selection. Add an event listener to the buttons that calls your playRound function with the correct playerSelection every time a button is clicked. (you can keep the console.logs for this step)
+Add a div for displaying results and change all of your console.logs into DOM methods.
+*/
+
 let playerScore = 0;
 let computerScore = 0;
+let computer = getComputerSelection();
+let winner = "";
+
 
 function getComputerSelection(){
-		    let choiceNum = Math.round(Math.random() * 2)
+		    let choiceNum = Math.round(Math.random() * 2);
 			let computerInput;
 			if (choiceNum === 0){
 			 computerInput = "Rock";
@@ -19,105 +28,107 @@ function getComputerSelection(){
 			return computerInput;
 }
 
- function getPlayerSelection(){
-	     let input = prompt ("What is your choice? Rock, paper, or scissors? Input is case insensitive");
-		 input.toLowerCase()
-		
-		  if (input != "rock" || input != "paper" || input != "scissors") {
-		    let input = prompt ("Sorry! Bad input! Let's try again. What is your choice? Rock, paper, or scissors?");
-		    return input;
-		}
-		else{
-		return input;
-		}
-}
-		
 
-function game(){
+function rock(){
 
-	let computer = getComputerSelection();
-    let player = getPlayerSelection();
-    let winner;
-	
-	
-	 if (player.toLowerCase() === "rock"){
-		     if (computer.toLowerCase() === "paper"){
-			    winner = "the computer";
-			 }
+	 if (computer.toLowerCase() === "paper"){
+			 winner = "the computer";
+			 computerScore++;
+	 }
 			 
-			 else if (computer.toLowerCase() === "scissors"){
-			      winner = "you!";
-			 }
+	 else if (computer.toLowerCase() === "scissors"){
+		winner = "you!";
+		playerScore++;
+	  }
 			 
-			  else if (computer.toLowerCase() === "rock"){
+	 else if (computer.toLowerCase() === "rock"){
 			      winner = "tied";
-			 }
-		  }/* end of if */
-		  
-		  if (player.toLowerCase() === "scissors"){
-		      if (computer.toLowerCase() === "paper"){
+				  playerScore++;
+				  computerScore++;
+	 }
+   }
+   
+   function paper(){
+	  
+        if (computer.toLowerCase() === "scissors"){
+			    winner = "the computer";
+				computerScore++;
+	    }
+			 
+	    else if (computer.toLowerCase() === "rock"){
+			      winner = "you!";
+				  playerScore++:
+			  }
+			  
+	    else if (computer.toLowerCase() === "paper"){
+			      winner = "tied";
+				  playerScore++;
+				  computerScore++;
+		}	  
+	   
+   }
+   
+    function scissors(){
+	  
+     if (computer.toLowerCase() === "paper"){
 			    winner = "you!";
+				playerScore++;
 			  }
 			 
-			 else if (computer.toLowerCase() === "rock"){
+	 else if (computer.toLowerCase() === "rock"){
 			      winner = "the computer";
-			  }
+				  computerScore++;
+	 }
 			  
-			  else if (computer.toLowerCase() === "scissors"){
-			      winner = "tied";
-			 }
-		  } /* end of scissors if */
-		  
-		  if (player.toLowerCase() === "paper"){
-		     if (computer.toLowerCase() === "scissors"){
-			    winner = "the computer";
-			  }
-			 
-			 else if (computer.toLowerCase() === "rock"){
-			      winner = "you!";
-			  }
-			  
-			  else if (computer.toLowerCase() === "paper"){
-			      winner = "tied";
-			  }
-		    } /* end of paper if */
-			
-			
-			if(winner != "tied"){
-			  alert("You chose:" + player.toLowerCase() + ". The computer chose: " + computer + ". The winner is: " + winner + "!" );
-			  
-			   if (winner === "the computer"){
-				   computerScore++;
-				   alert("The computer gets a point");
-			   }
-			   else if (winner === "you!"){
-				   playerScore++;
-				   alert("You get a point!");
-			   }
-			}
-			
-			else{
-			  alert("You chose: " + player + " The computer chose:  " + computer.toLowerCase() + ". You tied! You both get a point!"); 
-
-	
-			  playerScore++;
+	 else if (computer.toLowerCase() === "scissors"){
+			  winner = "tied";
 			  computerScore++;
-			}
-			
+			  playerScore++;
+	  }	    
+    }
+   
+const rockPic = document.createElement('div');
+const paperPic = document.createElement('div');
+const scissorsPic = document.createElement('div');
+const playerScoreWindow = document.createElement('div');
+const computerScoreWindow = document.createElement('div');
+
+rockPic.style.width = "500px";
+rockPic.style.height = "500px";
+paperPic.style.width = "500px"
+paperPic.style.height = "500px;";
+scissorsPic.style.width = "500px"
+scissorsPic.style.height = "500px"
+
+rockPic.style.backgroundImage = "url('rock.jpg')";
+rockPic.style.backgroundRepeat = "no-repeat";
+
+paperPic.style.backgroundImage = "url('paper.jpg')";
+paperPic.style.backgroundRepeat = "no-repeat";
+
+scissorsPic.style.backgroundImage = "url('scissors.jpg')";
+scissorsPic.style.backgroundRepeat = "no-repeat";
+
+document.body.appendChild(rockPic);
+document.body.appendChild(paperPic);
+document.body.appendChild(scissorsPic);
+
+rockPic.addEventListener('click', rock);
+
+paperPic.addEventListener('click', paper);
+
+scissorsPic.addEventListener('click', scissors);
+
+const playerScoreWindow.innerText = playerScore;
+const computerScoreWindow.innerText = computerScore;
 	
-            }/*end of function*/
-           for (let g = 0; g < 5; g++){
-	          game();
-           }
-		   
-		   alert("Player points: " + playerScore + "Computer points: " + computerScore);
-           if(playerScore > computerScore){
-			   alert("You win!!!");
-		   }
-		   else if (computerScore > playerScore){
-			   alert("The computer wins!");
-		   }
-		   
-		   else{
-			   alert("You tied!");
-		   }
+document.body.appendChild(playerScoreWindow);
+document.body.appendChild(computerScoreWindow);
+
+	 
+		  
+		  
+		  
+		 
+		  
+		 

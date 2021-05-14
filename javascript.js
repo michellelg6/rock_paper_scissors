bae0b5d828deb39f0b2ff717e24bccc76197a0f5
Michelle <michelle.liran@gmail.com>
2021-05-13 21:24:50 -0500

    put interface to html. Still nothing.

--- javascript.js
+++ javascript.js
@@ -7,128 +7,85 @@ Add a div for displaying results and change all of your console.logs into DOM me
 let playerScore = 0;
 let computerScore = 0;
 let computer = getComputerSelection();
+let player;
 let winner = "";
 
 
-function getComputerSelection(){
-		    let choiceNum = Math.round(Math.random() * 2);
-			let computerInput;
-			if (choiceNum === 0){
-			 computerInput = "Rock";
-			}
-			
-			if (choiceNum === 1){
-			 computerInput = "Scissors";
-			}
-			
-			if (choiceNum === 2){
-			 computerInput = "Paper";
-			}
-			
-			return computerInput;
+function getComputerSelection() {
+	let choiceNum = Math.round(Math.random() * 2);
+	let computerInput;
+	if (choiceNum === 0) {
+		computerInput = "Rock";
+	}
+
+	if (choiceNum === 1) {
+		computerInput = "Scissors";
+	}
+
+	if (choiceNum === 2) {
+		computerInput = "Paper";
+	}
+
+	return computerInput;
 }
 
 
-function rock(){
-
-	 if (computer.toLowerCase() === "paper"){
-			 winner = "the computer";
-			 computerScore++;
-	 }
-			 
-	 else if (computer.toLowerCase() === "scissors"){
+function rock() {
+   player = "rock"
+	if (computer.toLowerCase() === "paper") {
+		winner = "the computer";
+		computerScore++;
+	} else if (computer.toLowerCase() === "scissors") {
 		winner = "you!";
 		playerScore++;
-	  }
-			 
-	 else if (computer.toLowerCase() === "rock"){
-			      winner = "tied";
-				  playerScore++;
-				  computerScore++;
-	 }
-   }
-   
-   function paper(){
-	  
-        if (computer.toLowerCase() === "scissors"){
-			    winner = "the computer";
-				computerScore++;
-	    }
-			 
-	    else if (computer.toLowerCase() === "rock"){
-			      winner = "you!";
-				  playerScore++:
-			  }
-			  
-	    else if (computer.toLowerCase() === "paper"){
-			      winner = "tied";
-				  playerScore++;
-				  computerScore++;
-		}	  
-	   
-   }
-   
-    function scissors(){
-	  
-     if (computer.toLowerCase() === "paper"){
-			    winner = "you!";
-				playerScore++;
-			  }
-			 
-	 else if (computer.toLowerCase() === "rock"){
-			      winner = "the computer";
-				  computerScore++;
-	 }
-			  
-	 else if (computer.toLowerCase() === "scissors"){
-			  winner = "tied";
-			  computerScore++;
-			  playerScore++;
-	  }	    
-    }
-   
-const rockPic = document.createElement('div');
-const paperPic = document.createElement('div');
-const scissorsPic = document.createElement('div');
-const playerScoreWindow = document.createElement('div');
-const computerScoreWindow = document.createElement('div');
+	} else if (computer.toLowerCase() === "rock") {
+		winner = "tied";
+		playerScore++;
+		computerScore++;
+	}
+}
 
-rockPic.style.width = "500px";
-rockPic.style.height = "500px";
-paperPic.style.width = "500px"
-paperPic.style.height = "500px;";
-scissorsPic.style.width = "500px"
-scissorsPic.style.height = "500px"
+function paper() {
+    player = "paper"
+	if (computer.toLowerCase() === "scissors") {
+		winner = "the computer";
+		computerScore++;
+	} else if (computer.toLowerCase() === "rock") {
+		winner = "you!";
+		playerScore++;
+	} else if (computer.toLowerCase() === "paper") {
+		winner = "tied";
+		playerScore++;
+		computerScore++;
+	}
 
-rockPic.style.backgroundImage = "url('rock.jpg')";
-rockPic.style.backgroundRepeat = "no-repeat";
+}
 
-paperPic.style.backgroundImage = "url('paper.jpg')";
-paperPic.style.backgroundRepeat = "no-repeat";
+function scissors() {
+    player = "scissors"
+	if (computer.toLowerCase() === "paper") {
+		winner = "you!";
+		playerScore++;
+	} else if (computer.toLowerCase() === "rock") {
+		winner = "the computer";
+		computerScore++;
+	} else if (computer.toLowerCase() === "scissors") {
+		winner = "tied";
+		computerScore++;
+		playerScore++;
+	}
+}
 
-scissorsPic.style.backgroundImage = "url('scissors.jpg')";
-scissorsPic.style.backgroundRepeat = "no-repeat";
+document.getElementById("#rock").addEventListener('click', rock);
 
-document.body.appendChild(rockPic);
-document.body.appendChild(paperPic);
-document.body.appendChild(scissorsPic);
+document.getElementById("#paper").addEventListener('click', paper);
 
-rockPic.addEventListener('click', rock);
+document.getElementById("#scissors").addEventListener('click', scissors);
 
-paperPic.addEventListener('click', paper);
+document.getElementById("#playerOption").textContent(player);
+document.getElementById("#computerOption").textContent(computer);
 
-scissorsPic.addEventListener('click', scissors);
+document.getElementById("#playerScoreWindow").textContent(playerScore.toString());
+document.getElementById("#computerScoreWindow").textContent(computerScore.toString());
 
-const playerScoreWindow.innerText = playerScore;
-const computerScoreWindow.innerText = computerScore;
-	
-document.body.appendChild(playerScoreWindow);
-document.body.appendChild(computerScoreWindow);
-
-	 
-		  
-		  
-		  
-		 
-		  
-		 
+document.getElementById("#winner").textContent(winner);

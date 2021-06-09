85a8afbee4c4d9a5bb49f6e0dbd977e87ff4422a
Michelle <michelle.liran@gmail.com>
2021-06-09 16:55:41 -0500

    changed button selector in javascript to target the divs with images

--- javascript.js
+++ javascript.js
@@ -1,169 +1,111 @@
-/*
-
-Create three buttons, one for each selection. Add an event listener to the buttons that calls your playRound function with the correct playerSelection every time a button is clicked. (you can keep the console.logs for this step)
-Add a div for displaying results and change all of your console.logs into DOM methods.
-*/
-
-
-/*Initialized the scores*/
 let playerScore = 0;
 let computerScore = 0;
-let roundNum = 0;
 
-
-/*set variable to computer's selection*/
-let computer = getComputerSelection();
-
-
-/*to store who the winner is*/
-let winner = "";
-
-
-function getComputerSelection() {
+function computerPlay(){
+	
 	let choiceNum = Math.floor(Math.random() * 2);
-	let computerInput;
-	if (choiceNum === 0) {
-		computerInput = "Rock";
-	}
+	     let computerSelection;
+	     if (choiceNum === 0) {
+		 computerSelection = "rock";
+	     }
 
-	if (choiceNum === 1) {
-		computerInput = "Scissors";
-	}
+	     if (choiceNum === 1) {
+		 computerSelection = "scissors";
+	     }
 
-	if (choiceNum === 2) {
-		computerInput = "Paper";
-	}
-
-	return computerInput;
-}
-
-function getWinner(winner){
-  if (computerScore === playerScore){
-	  winner = "tied";
-  }
-  
-  else if (computerScore > playerScore){
-	  winner = "the computer"
-  }
-  
-  else if (playerScore > computerScore){
-	  winner = "you"
-  }
-  
-  return winner;
-}
-
-function winnerScreen(){
+	     if (choiceNum === 2) {
+		 computerSelection = "paper";
+	     }	
+		 
+		 return computerSelection;
 	
-let choices = document.getElementById("choices");
-let resultingSelections = document.getElementById("resultingSelections");
-theRound = document.getElementById("roundNum");
-	
-document.getElementById("winner").innerHTML = "<h2>And the winner is...... " + getWinner(winner) + "!</h2>";
-document.getElementById("playerSelection").removeChild(choices);
-document.getElementById("results").removeChild(resultingSelections);
-document.getElementById("myBody").removeChild(theRound);
-
 }
 
-function showData() {
+function playRound(playerSelection, computerSelection)
+{
 
-	document.getElementById("playerScoreWindow").innerHTML = "<h3>Player Score: " + playerScore.toString() + "</h3>";
-	document.getElementById("computerScoreWindow").innerHTML = "<h3>Computer Score: " + computerScore.toString() + "</h3>";
+let result;
 
 
+if(playerSelection === computerSelection){
+		result = "You chose: " + playerSelection + " and the computer chose " + computerSelection + ". You tied!";	
+		return result;
+		playerScore++;
+		computerScore++;
+}	
+
+else if(playerSelection === "rock") {
+   
+		if (computerSelection === "paper") {
+			computerScore++;
+			result = "You chose: " + playerSelection + " and the computer chose " + computerSelection + ". Paper covers rock! The computer scores!!";
+			return result;
+		} else if ( computerSelection === "scissors") {
+			playerScore++;
+			result = "You chose: " + playerSelection + " and the computer chose " + computerSelection + ". Rock breaks scissors! You score!";	
+			return result;
+         } 	
 }
 
-function rock() {
-    
-	if (roundNum < 5) {
-		document.getElementById("playerOption").innerHTML = "<h3>You chose Rock</h3>";
-		document.getElementById("computerOption").innerHTML = "<h3>The computer chose " + getComputerSelection() + "</h3>";
-
-
-
-		if (computer.toLowerCase() === "paper") {
+else if(playerSelection === "paper") {
+   
+		if ( computerSelection === "scissors") {
 			computerScore++;
-			showData();
-		} else if (computer.toLowerCase() === "scissors") {
+			results =  "You chose: " + playerSelection + " and the computer chose " + computerSelection + "<h3>Scissors cuts paper. The computer scores!";
+			return result;
+		} else if ( computerSelection === "rock") {
 			playerScore++;
-			showData();
-		} else if (computer.toLowerCase() === "rock") {
-			playerScore++;
-			computerScore++;
-			showData();
-		}
-
-        roundNum++;
-		document.getElementById('roundNum').innerHTML = '<h3>Round ' + roundNum + '</h3>';
-		
-	} else {
-		winnerScreen();
-	}
+			result = "You chose: " + playerSelection + " and the computer chose " + computerSelection + ". Paper covers rock.You score!";
+			return result;
+         } 	
 }
 
-
-function paper() {
-
-	if (roundNum < 5) {
-		/*Displaying the choice*/
-		document.getElementById("playerOption").innerHTML = "<h3>You chose paper</h3>";
-		document.getElementById("computerOption").innerHTML = "<h3>The computer chose: " + getComputerSelection().toString() + "</h3>";
-		if (computer.toLowerCase() === "scissors") {
+else if(playerSelection === "scissors") {
+   
+		if ( computerSelection === "rock") {
 			computerScore++;
-			showData();
-		} else if (computer.toLowerCase() === "rock") {
+			result = "You chose: " + playerSelection + " and the computer chose " + computerSelection + ". Rock breaks scissors. The computer scores!!";
+			return result;
+		} else if ( computerSelection === "paper") {
 			playerScore++;
-			showData();
-		} else if (computer.toLowerCase() === "paper") {
-			playerScore++;
-			computerScore++;
-			showData();
-		}
-        roundNum++;
-		document.getElementById('roundNum').innerHTML = '<h3>Round ' + roundNum + '</h3>';
-		
-	} else {
-		winnerScreen();
-		
-	}
-
+			result = "You chose: " + playerSelection + " and the computer chose " + computerSelection + ". Scissors cuts paper. You score!";
+			return result;
+         } 		 
+}    
 }
 
-function scissors() {
-     
-	if (roundNum < 5) {
+/*function game(){*/
+var round;
 
-		document.getElementById("playerOption").innerHTML = "<h3>You chose Scissors!</h3>";
-		document.getElementById("computerOption").innerHTML = "<h3>The computer chose: " + getComputerSelection().toString() + "</h3>";
-		if (computer.toLowerCase() === "paper") {
-			playerScore++;
-			showData();
-		} else if (computer.toLowerCase() === "rock") {
-			computerScore++;
-			showData();
-		} else if (computer.toLowerCase() === "scissors") {
-			computerScore++;
-			playerScore++;
-			showData();
-		}
-         roundNum++;
-		
-		document.getElementById('roundNum').innerHTML = '<h3>Round ' + roundNum + '</h3>';
-	} else {
+/*for(round = 1; round <= 5; round++){*/
 
-		winnerScreen();
-		
-	}
+//variable for all buttons
+const buttons = document.querySelectorAll('.selection');
 
+
+buttons.forEach((button) => {
+	let computerSelection = computerPlay();
+    let playerSelection = button.id;
+	button.addEventListener("click", () => { 
+	console.log(playRound(playerSelection, computerSelection)); 
+	console.log("The player's score is :" + playerScore);
+    console.log("The computer's score is :" + computerScore);
+  });
+});
+
+
+/*console.log("*The player's score finale is: " + playerScore);
+console.log("*The computer's score finale is: " + computerScore);
+
+if(playerScore > computerScore){
+	console.log("You won!!!");
 }
+else if(computerScore > playerScore){
+console.log("The computer won!");}
 
+else if (playerScore === computerScore){
+	console.log("You tied!");
+}
+/*}*/
 
-const rockButton = document.querySelector('#rock');
-rockButton.addEventListener('click', rock);
-
-const paperButton = document.querySelector('#paper');
-paperButton.addEventListener('click', paper);
-
-const scissorsButton = document.querySelector('#scissors');
-scissorsButton.addEventListener('click', scissors);
+/*game();*/

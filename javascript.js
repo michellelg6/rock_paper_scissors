f49025bb92eba1780b9d8a7a8e30e96248d89e96
Michelle <michelle.liran@gmail.com>
2021-06-28 21:43:26 -0500

    redid scores logic and layout

--- javascript.js
+++ javascript.js
@@ -1,5 +1,6 @@
 let playerScore = 0;
 let computerScore = 0;
+let round = 1;
 
 function computerPlay(){
 	
@@ -32,6 +33,7 @@ if(playerSelection === computerSelection){
 		return result;
 		playerScore++;
 		computerScore++;
+		
 }	
 
 else if(playerSelection === "rock") {
@@ -40,10 +42,12 @@ else if(playerSelection === "rock") {
 			computerScore++;
 			result = "You chose: " + playerSelection + " and the computer chose " + computerSelection + ". Paper covers rock! The computer scores!!";
 			return result;
+			
 		} else if ( computerSelection === "scissors") {
 			playerScore++;
 			result = "You chose: " + playerSelection + " and the computer chose " + computerSelection + ". Rock breaks scissors! You score!";	
 			return result;
+			
          } 	
 }
 
@@ -51,7 +55,7 @@ else if(playerSelection === "paper") {
    
 		if ( computerSelection === "scissors") {
 			computerScore++;
-			results =  "You chose: " + playerSelection + " and the computer chose " + computerSelection + "<h3>Scissors cuts paper. The computer scores!";
+			result =  "You chose: " + playerSelection + " and the computer chose " + computerSelection + "Scissors cuts paper. The computer scores!";
 			return result;
 		} else if ( computerSelection === "rock") {
 			playerScore++;
@@ -74,24 +78,77 @@ else if(playerSelection === "scissors") {
 }    
 }
 
-/*function game(){*/
-var round;
+function displayScoreTableRow(){
+	let roundRow = document.createElement('tr');
+	let roundCell = document.createElement('td');
+	let playerScoreCell = document.createElement('td');
+	let compScoreCell = document.createElement('td');
+	
+	roundCell.textContent = round;
+	playerScoreCell.textContent = playerScore;
+	compScoreCell.textContent = computerScore;
+	
+	let tbl = document.querySelector('table');
+	tbl.appendChild(roundRow);
+	roundRow.appendChild(roundCell);
+	roundRow.appendChild(playerScoreCell);
+	roundRow.appendChild(compScoreCell);
+}
+
+function winnerScreen(){
+	let winner = "";
+	if (playerScore > computerScore){
+		winner = "you!!! Congratulations, you won!";
+	}
+	
+	else if (computerScore > playerScore){
+	   winner = "the computer! Sorry, you lose!";	
+	}
+	
+	else if (computerScore === playerScore){
+		winner = "Nobody! It's a tie!";
+	}
+	
+	let finalResults = document.getElementById("results");
+	let playersChoice = document.getElementById("playerSelection");
+	finalResults.removeChild(document.getElementById("showResults"));
+	playersChoice.removeChild(document.getElementById("showPlayerSelection"));
+	document.getElementById("winner").textContent = 'And the winner is....' + winner;
+}
+
 
 /*for(round = 1; round <= 5; round++){*/
 
 //variable for all buttons
 const buttons = document.querySelectorAll('.selection');
+/*const rounds = document.querySelectorAll('.round');*/
 
+/*do{*/
 
-buttons.forEach((button) => {
+	buttons.forEach((button) => {
+	
 	let computerSelection = computerPlay();
     let playerSelection = button.id;
 	button.addEventListener("click", () => { 
-	console.log(playRound(playerSelection, computerSelection)); 
-	console.log("The player's score is :" + playerScore);
-    console.log("The computer's score is :" + computerScore);
+	
+	if(round <= 5){
+		document.getElementById("displaySelections").textContent = playRound(playerSelection, computerSelection); 
+		document.getElementById('roundNum').textContent = "Round " + round.toString();
+		document.getElementById("announcePlayerScore").textContent = "The player's score is :" + playerScore.toString();
+		document.getElementById("announceComputerScore").textContent = "The computer's score is :" + computerScore.toString();
+		displayScoreTableRow();
+	}
+	else{
+		winnerScreen();
+    }
+	
+    	
+	round++;
+		 
   });
 });
+/*} while(round <= 5);*/
+
 
 
 /*console.log("*The player's score finale is: " + playerScore);
